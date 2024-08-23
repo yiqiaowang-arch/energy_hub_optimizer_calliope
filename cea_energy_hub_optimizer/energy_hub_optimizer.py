20666ef1562586c41e300db2a0b8aa826e04e198
Yiqiao Wang <yiqwang@student.ethz.ch>
2024-08-23 18:41:50 +0200

    Add additional input check for solar technologies that are selected by user in plugin.config.

--- cea_energy_hub_optimizer/energy_hub_optimizer.py
+++ cea_energy_hub_optimizer/energy_hub_optimizer.py
@@ -39,13 +39,14 @@ def main(config: cea.config.Configuration):
     :param cea.config.Configuration config: The configuration for this script, restricted to the scripts parameters.
     :return: None
     """
+    check_solar_technology(config)
     warnings.filterwarnings('ignore')
     locator = cea.inputlocator.InputLocator(config.scenario)
     buildings: list[str] = config.energy_hub_optimizer.buildings
     yaml_path = os.path.join(os.path.dirname(__file__), 'data', 'techs_energy_hub.yml')
     store_folder: str = locator._ensure_folder(locator.get_optimization_results_folder(), 'calliope_energy_hub')
     calliope.set_log_verbosity(verbosity='error', include_solver_output=False, capture_warnings=False)
-    # comments on _ensure_folder:
+    # comments on _ensure_folder: 
     # Return the *components joined together as a path to a folder and ensure that that folder exists on disc. 
     # If it doesn't exist yet, attempt to make it with os.makedirs.
     for building in buildings:
@@ -73,6 +74,54 @@ def main(config: cea.config.Configuration):
         df_pareto_aug.to_csv(store_folder+'/'+building_name+'_pareto.csv', index=True, index_label='index')
         print(building_name+' is optimized! Results saved in ' + store_folder)
         del energy_hub
+
+
+def check_solar_technology(config: cea.config.Configuration):
+    """
+    Check if the solar technology has been pre-evaluated by CEA. If not, raise an error.
+    This function replaced the input check in the script.yml file, because there one needs to specify the technologies 
+    beforehand. In our script, we have the freedom to include the technologies we want to evaluate, so we need to read
+    the choices first and check them more dynamically.
+
+    On the other hand, the demand of each building must be available anyway, so we don't need to check that in the function.
+    Instead, scripts.yml will check if each building has a demand profile csv available.
+
+    :param cea.config.Configuration config: The configuration for this script, restricted to the scripts parameters.
+    """
+    tech_list = config.energy_hub_optimizer.evaluated_solar_supply
+    print(f"""
+          Checking if solar technology already has been pre-evaluated by CEA...
+          Evaluated solar supply: {tech_list}
+          """)
+    locator = cea.inputlocator.InputLocator(config.scenario)
+    buildings: list[str] = config.energy_hub_optimizer.buildings
+    errors = []
+
+    for building in buildings:
+        if "PV" in tech_list:
+            if not os.path.exists(locator.PV_results(building)):
+                errors.append(f"{building} does not have PV results in the scenario folder.")
+                
+        if "PVT" in tech_list:
+            if not os.path.exists(locator.PVT_results(building)):
+                errors.append(f"{building} does not have PVT results in the scenario folder.")
+                
+        if "SCET" in tech_list:
+            if not os.path.exists(locator.SC_results(building, 'ET')):
+                errors.append(f"{building} does not have SCET results in the scenario folder.")
+                
+        if "SCFP" in tech_list:
+            if not os.path.exists(locator.SC_results(building, 'FP')):
+                errors.append(f"{building} does not have SCFP results in the scenario folder.")
+                
+    if len(errors) > 0:
+        for error in errors:
+            print(error)
+        print("Not all buildings have results for the solar technologies used in optimization. Script aborted.")
+        raise FileNotFoundError("Not all buildings have results for the solar technologies used in optimization. Script aborted.")
+    
+    print("All buildings have results for the solar technologies. Continue with the optimization.")
+
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
