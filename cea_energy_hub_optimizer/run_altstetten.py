a96c7228f5409e7414945208f54032492cfb3ffb
Yiqiao Wang <yiqwang@student.ethz.ch>
2025-01-19 19:32:36 +0100

    update run_altstetten.py to start new testing

--- cea_energy_hub_optimizer/run_altstetten.py
+++ cea_energy_hub_optimizer/run_altstetten.py
@@ -104,14 +104,14 @@ check_solar_technology()
 zone: pd.DataFrame = gpd.read_file(locator.get_zone_geometry(), ignore_geometry=True)
 # get all the building names
 buildings = zone["Name"].tolist()
-result_folder = os.path.join(locator.get_optimization_results_folder(), "calliope_energy_hub", "batch_no_oil_with_DH")
+result_folder = os.path.join(locator.get_optimization_results_folder(), "calliope_energy_hub", "sbe_test_HP_price")
 scenario_folder = locator.scenario
 # read DH_availability.csv and get the list of buildings that have access to district heating
 DH_availability = pd.read_csv(os.path.join(scenario_folder, "DH_availability.csv"))
 ls_DH_available = DH_availability["Name"].tolist()
 if not os.path.exists(result_folder):
     os.makedirs(result_folder)
-config_path = (r"cea_energy_hub_optimizer\data\energy_hub_config_emission_sensitivity.yml")
+config_path = (r"cea_energy_hub_optimizer\data\energy_hub_config.yml")
 
 # define buildings that belongs to a district energy network
 network_149 = [ "B302062896", "B302062895", "B302062776", "B302062775", "B302060244", "B302060243", "B302060242", "B302030224", "B302030223", "B161046", 
@@ -160,7 +160,7 @@ for building_name in buildings:
     if building_name not in ls_DH_available:
         remove_district_heating_technologies(energy_hub)
     # for now we keep oil technologies so the following line is commented
-    remove_oil_technologies(energy_hub)
+    # remove_oil_technologies(energy_hub)
     # remove_gas_technologies(energy_hub)
     # remove_pallet_technologies(energy_hub)
     energy_hub.get_pareto_front(store_folder=result_folder)
@@ -172,4 +172,4 @@ for building_name in buildings:
     )
     print(building_name + " is optimized! Results saved in " + result_folder)
     del energy_hub
-    gc.collect()
+    # gc.collect()
